78773d7b208196fed5139b7c898a3f09aae415cc
denis-donda <denishenriquedonda@gmail.com>
2022-06-22 07:47:41 -0300

    Pequena Refatoração no BookController, implementando Biblioteca ModelMapper para simplificar builders.

--- src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
@@ -1,10 +1,17 @@
 package com.cursodsousa.libraryapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryApiApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){ // Cria um instância Singleton de ModelMapper para servir toda aplicação.
+		return new ModelMapper();
+	};
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
--- src/main/java/com/cursodsousa/libraryapi/api/resource/BookController.java
+++ src/main/java/com/cursodsousa/libraryapi/api/resource/BookController.java
@@ -3,6 +3,7 @@ package com.cursodsousa.libraryapi.api.resource;
 import com.cursodsousa.libraryapi.api.dto.BookDTO;
 import com.cursodsousa.libraryapi.api.model.entity.Book;
 import com.cursodsousa.libraryapi.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,27 +11,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/books")
 public class BookController {
 
-    private BookService service;
+    private final BookService service;
+    private ModelMapper modelMapper;
 
-    public BookController(BookService service) {
+    public BookController(BookService service, ModelMapper mapper) {
         this.service = service;
+        this.modelMapper = mapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto) {
-        Book entity = Book.builder()
-                .author(dto.getAuthor())
-                .title(dto.getTitle())
-                .isbn(dto.getIsbn())
-                .build();
+        Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
-        return BookDTO.builder()
-                .id(entity.getId())
-                .author(entity.getAuthor())
-                .title(entity.getTitle())
-                .isbn(entity.getIsbn())
-                .build();
+        return modelMapper.map(entity, BookDTO.class);
     }
 
 }
