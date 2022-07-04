1a459ae3bd9bda927a8f21bc6c9ecbf8c0ad5cdb
denis-donda <denishenriquedonda@gmail.com>
2022-07-04 08:16:25 -0300

    Implementando 2 testes de Integração com H2.

--- src/test/java/com/cursodsousa/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/cursodsousa/libraryapi/model/repository/BookRepositoryTest.java
@@ -1,4 +1,54 @@
 package com.cursodsousa.libraryapi.model.repository;
 
+import com.cursodsousa.libraryapi.api.model.entity.Book;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("teste")
+@DataJpaTest
 public class BookRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    BookRepository repository;
+
+    @Test
+    @DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
+    public void returnTrueWhenIsbnExists(){
+        //arranje
+        String isbn = "123";
+        Book book = Book.builder().title("Aventuras").author("Fulano").isbn(isbn).build();
+        entityManager.persist(book);
+
+        //act
+        boolean exists = repository.existsByIsbn(isbn);
+
+        //assert
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar false quando não existir um livro na base com o isbn informado")
+    public void returnFalseWhenIsbnDoesntExist(){
+        //arranje
+        String isbn = "123";
+
+        //act
+        boolean exists = repository.existsByIsbn(isbn);
+
+        //assert
+        assertThat(exists).isFalse();
+    }
+
 }
