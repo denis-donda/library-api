e216f49fc2d772793508f8ddfd2764ebc1c7c5c7
denis-donda <denishenriquedonda@gmail.com>
2022-07-04 21:28:45 -0300

    Implementando teste getByIdTest() em BookServiceTest.

--- src/test/java/com/cursodsousa/libraryapi/service/BookServiceTest.java
+++ src/test/java/com/cursodsousa/libraryapi/service/BookServiceTest.java
@@ -4,17 +4,25 @@ import com.cursodsousa.libraryapi.api.exception.BusinessException;
 import com.cursodsousa.libraryapi.api.model.entity.Book;
 import com.cursodsousa.libraryapi.model.repository.BookRepository;
 import com.cursodsousa.libraryapi.service.impl.BookServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -70,6 +78,26 @@ public class BookServiceTest { //Apenas testes unitários
                 .hasMessage("Isbn já cadastrado.");
 
         Mockito.verify(repository, Mockito.never()).save(book); //Nunca deve chamar o save(book).
+    }
+
+    @Test
+    @DisplayName("GET - Deve obter um livro por Id.")
+    public void getByIdTest(){
+        //arrange
+        Long id = 1L;
+        Book book = createValidBook(Book.builder());
+        book.setId(id);
+        Mockito.when(repository.findById(id)).thenReturn(Optional.of(book));
+
+        //act
+        Optional<Book> foundBook = service.getById(id);
+
+        //assert
+        assertThat(foundBook.isPresent()).isTrue();
+        assertThat(foundBook.get().getId()).isEqualTo(id);
+        assertThat(foundBook.get().getAuthor()).isEqualTo(book.getAuthor());
+        assertThat(foundBook.get().getIsbn()).isEqualTo(book.getIsbn());
+        assertThat(foundBook.get().getTitle()).isEqualTo(book.getTitle());
     }
 
 }
