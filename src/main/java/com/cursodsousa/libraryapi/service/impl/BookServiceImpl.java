de3bbf030362f41c2329f3caacf064dce7fa89ad
denis-donda <denishenriquedonda@gmail.com>
2022-07-05 00:39:18 -0300

    Implementando Método findBookTest() em BookServiceTest.

--- src/main/java/com/cursodsousa/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/cursodsousa/libraryapi/service/impl/BookServiceImpl.java
@@ -4,6 +4,8 @@ import com.cursodsousa.libraryapi.api.exception.BusinessException;
 import com.cursodsousa.libraryapi.api.model.entity.Book;
 import com.cursodsousa.libraryapi.model.repository.BookRepository;
 import com.cursodsousa.libraryapi.service.BookService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -51,6 +53,13 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Page<Book> find(Book filter, Pageable pageRequest) {
-        return null;
+        Example<Book> example = Example.of(filter,
+                ExampleMatcher
+                        .matching()
+                        .withIgnoreCase()
+                        .withIgnoreNullValues()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING) //Busca os livros que contenham...
+        );
+        return repository.findAll(example, pageRequest);
     }
 }
