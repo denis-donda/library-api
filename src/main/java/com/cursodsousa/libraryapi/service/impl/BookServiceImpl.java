1bac8a0fbbfac3501db693e6d267427e335944b9
denis-donda <denishenriquedonda@gmail.com>
2022-07-05 00:13:32 -0300

    Implementando teste findBooksTest() em BookControlerTest.

--- src/main/java/com/cursodsousa/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/cursodsousa/libraryapi/service/impl/BookServiceImpl.java
@@ -4,6 +4,8 @@ import com.cursodsousa.libraryapi.api.exception.BusinessException;
 import com.cursodsousa.libraryapi.api.model.entity.Book;
 import com.cursodsousa.libraryapi.model.repository.BookRepository;
 import com.cursodsousa.libraryapi.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -45,5 +47,10 @@ public class BookServiceImpl implements BookService {
             throw new IllegalArgumentException("Book id cant be null.");
         }
         return repository.save(book);
+    }
+
+    @Override
+    public Page<Book> find(Book filter, Pageable pageRequest) {
+        return null;
     }
 }
--- src/test/java/com/cursodsousa/libraryapi/api/resource/BookControlerTest.java
+++ src/test/java/com/cursodsousa/libraryapi/api/resource/BookControlerTest.java
@@ -5,6 +5,7 @@ import com.cursodsousa.libraryapi.api.model.entity.Book;
 import com.cursodsousa.libraryapi.api.exception.BusinessException;
 import com.cursodsousa.libraryapi.service.BookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.h2.mvstore.Page;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,6 +26,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -243,6 +248,38 @@ public class BookControlerTest {
         mvc
                 .perform(request)
                 .andExpect(status().isNotFound())
+        ;
+    }
+
+    @Test
+    @DisplayName("Deve filtrar livros")
+    public void findBooksTest() throws Exception{
+        Long id = 1L;
+
+        Book book = Book.builder()
+                .id(id)
+                .title(createNewBook().getTitle())
+                .author(createNewBook().getAuthor())
+                .isbn(createNewBook().getAuthor())
+                .build();
+
+        BDDMockito.given(service.find(Mockito.any(Book.class), Mockito.any(Pageable.class)))
+                .willReturn(new PageImpl<Book>(Arrays.asList(book), PageRequest.of(0, 100), 1));
+
+        String queryString = String.format("?title=%s&author=%s&page=0&size=100",
+                book.getTitle(), book.getAuthor());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get(BOOK_API.concat(queryString))
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("content", Matchers.hasSize(1)))
+                .andExpect(jsonPath("totalElements").value(1))
+                .andExpect(jsonPath("pageable.pageSize").value(100))
+                .andExpect(jsonPath("pageable.pageNumber").value(0))
         ;
     }
 
