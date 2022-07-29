9d03140781af1f2cd0f93eea9363c7317060ba1d
denis-donda <denishenriquedonda@gmail.com>
2022-07-29 09:11:23 -0300

    Implementando EmailServiceImpl.

---
+++ src/main/java/com/cursodsousa/libraryapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cursodsousa.libraryapi.service.impl;
+
+import com.cursodsousa.libraryapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendMails(String message, List<String> mailsList) {
+
+    }
+}
